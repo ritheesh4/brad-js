dc52df293675cfcf2ba24aa18dff8fb0cd6015d4
ritheesh4 <ritheesh.4@gmail.com>
2020-06-10 12:41:30 +0530

    video 7 completed

---
+++ 02_js_fundamentals/07_app.js
@@ -0,0 +1,55 @@
+// // var,let const
+
+// var name ="Jone Doe";
+// console.log(name);
+// name ="Steve smith";
+// console.log(name);
+
+// //Int var
+// var greeting;
+// console.log(greeting);
+// greeting = "hello";
+// console.log(greeting);
+
+// // letters, numbers, _,$
+// // Can not start with number
+
+// var name ="John";
+
+// // Multi word vars
+// var firstName ="John"; //Camel case
+// var first_name ="sara" //Underscore
+// var FirstName = 'Tom'; // Pascal 
+
+// LET
+// let name;
+// let name ="Jone Doe";
+// console.log(name);
+// name ="Steve smith";
+// console.log(name);
+
+// CONST
+
+// const name ="Jon";
+// console.log(name);
+// name ="christy";
+// const greeting;
+
+const person ={
+    name:'John',
+    age:30
+}
+
+person.name ="Allen";
+person.age ="32";
+
+// console.log(person);
+
+const numbers =[1,2,3,4,5];
+numbers.push(6);
+numbers =[1,2,3,4];
+
+console.log(numbers);
+
+
+ 
