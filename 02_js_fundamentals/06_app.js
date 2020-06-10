2ee4774e3711ad924ae8a9228f0156d7ab4d1f08
ritheesh4 <ritheesh.4@gmail.com>
2020-06-10 12:19:31 +0530

    video5 and video6 completed

---
+++ 02_js_fundamentals/06_app.js
@@ -0,0 +1,21 @@
+// Log to console
+console.log("hello world");
+console.log(123);
+console.log(true);
+var greeting ="hello";
+console.log([1,2,3,4,5]);
+console.log({a:1,b:3,c:2});
+console.table({a:1,b:2});
+console.error("This is some error");
+console.clear();
+console.warn("This is a warning");
+console.time("hello");
+  console.log("Hello world");
+  console.log("Hello world");
+  console.log("Hello world");
+  console.log("Hello world");
+  console.log("Hello world");
+  console.log("Hello world");
+  console.log("Hello world");
+  console.log("Hello world");
+console.timeEnd("hello");
