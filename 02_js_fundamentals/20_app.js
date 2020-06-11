6a4808c887b6fd6f5da63740e0a0fc65d4194c04
ritheesh4 <ritheesh.4@gmail.com>
2020-06-11 13:32:11 +0530

    Video 20 completed

---
+++ 02_js_fundamentals/20_app.js
@@ -0,0 +1,59 @@
+//  WINDOW METHODS / OBJECTS / PROPERTIES
+
+// console.log(123);
+
+// Alert
+// alert('Hello world');
+
+// Prompt
+// const input = prompt();
+// altert(input);
+
+// Confirm
+
+// if(confirm('Are you sure')){
+//     console.log('YES');
+// } else {
+//     console.log('NO');
+// }
+
+let val;
+
+// Outer height and width
+val = window.outerHeight;
+val = window.outerWidth;
+
+// Scroll points
+val = window.scrollY;
+val = window.scrollX;
+
+// Location Object
+val = window.location;
+val = window.location.hostname;
+val = window.location.port;
+val = window.location.href;
+val = window.location.search;
+
+//Redirect
+// window.location.href = "http://google.com";
+//Reload
+// window.location.reload();
+
+// History Object
+// window.history.go(-2);
+// val = window.history.length;
+
+// Navigator Object
+val = window.navigator;
+val = window.navigator.appName;
+val = window.navigator.appVersion;
+val = window.navigator.userAgent;
+val = window.navigator.platform;
+val = window.navigator.vendor;
+console.log(val);
+
+
+
+
+
+
