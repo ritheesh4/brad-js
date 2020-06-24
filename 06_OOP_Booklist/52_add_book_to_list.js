02b65389c09d0ca0c458cc5d519fa692af15d680
ritheesh4 <ritheesh.4@gmail.com>
2020-06-24 08:35:52 +0530

    video52 completed

--- 06_OOP_Booklist/52_add_book_to_list.js
+++ 06_OOP_Booklist/52_add_book_to_list.js
@@ -9,18 +9,34 @@ function Book(title, author, isbn) {
 function UI() { }
 
 // Add Book To List
-UI.prototype.addBookToList = function(book){
+UI.prototype.addBookToList = function (book) {
     const list = document.getElementById('book-list');
     // Create tr element
-    
+    const row = document.createElement('tr');
+    // Insert cols
+    row.innerHTML = `
+        <td>${book.title}</td>
+        <td>${book.author}</td>
+        <td>${book.isbn}</td>
+        <td><a href ="#" class = "delete">X</a></td>`
+        ;
+
+    list.appendChild(row);
+}
+
+// Clear Fields
+UI.prototype.clearFields = function (){
+    document.getElementById('title').value = '';
+    document.getElementById('author').value = '';
+    document.getElementById('isbn').value = '';
 }
 
 // Event Listeners
 document.getElementById('book-form').addEventListener('submit', function (e) {
     // Get form values
-    const title =  document.getElementById('title').value,
-          author = document.getElementById('author').value,
-          isbn = document.getElementById('isbn').value;
+    const title = document.getElementById('title').value,
+        author = document.getElementById('author').value,
+        isbn = document.getElementById('isbn').value;
 
     // Instantiate book
     const book = new Book(title, author, isbn);
@@ -30,7 +46,11 @@ document.getElementById('book-form').addEventListener('submit', function (e) {
 
     console.log(ui);
 
-    // Add book
+    // Add book tolist
     ui.addBookToList(book)
+
+    // Clear fields
+    ui.clearFields();
+
     e.preventDefault();
 });
