694f959c56d0c4f89e995da59c38977193b8da93
ritheesh4 <ritheesh.4@gmail.com>
2020-06-26 16:34:47 +0530

    promise section completed

---
+++ 07_asyncJs_ajax/66_es6_promises.js
@@ -0,0 +1,56 @@
+const posts = [
+    { title: 'Post One', body: 'This is post one' },
+    { title: 'Post Two', body: 'Thisd is post two' }
+];
+
+// function createPost(post) {
+//     setTimeout(function () {
+//         posts.push(post);
+//     }, 2000);
+// }
+
+// function getPosts() {
+//     setTimeout(function () {
+//         let output = '';
+//         posts.forEach(function (post) {
+//             output += `<li>${post.title}</li>`;
+//         });
+//         document.body.innerHTML = output;
+//     }, 1000);
+// }
+
+// createPost({ title: 'Post THree', body: 'This is post three' });
+
+// getPosts();
+
+
+function createPost(post) {
+    return new Promise(function (resolve, reject) {
+        setTimeout(function () {
+            posts.push(post);
+            const error = false;
+            if (!error) {
+                resolve();
+            } else {
+                reject("Error: Something went wrong");
+
+            }
+        }, 2000);
+    });
+
+}
+
+function getPosts() {
+    setTimeout(function () {
+        let output = '';
+        posts.forEach(function (post) {
+            output += `<li>${post.title}</li>`;
+        });
+        document.body.innerHTML = output;
+    }, 1000);
+}
+
+createPost({ title: 'Post Three', body: 'This is post three' }).then(getPosts).catch(function (err) {
+    console.log(err);
+});
+
