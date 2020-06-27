91acb3f1797923cf880632cdd41ffec4e6d0f59b
ritheesh4 <ritheesh.4@gmail.com>
2020-06-27 16:39:28 +0530

    ui weather part3 completed

---
+++ 08_api_projects/80_weather_part3.js
@@ -0,0 +1,22 @@
+class Weather {
+    constructor(city, state) {
+        this.apiKey = '50fa63a7f93c025626b511d658966940';
+        this.city = city;
+        this.state = state;
+    }
+
+
+    // Getch weather from API
+    async getWeather() {
+        const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${this.city},${this.state}&appid=${this.apiKey}`);
+
+        const responseData = await response.json();
+        return responseData;
+    }
+
+    // Change weather location
+    changeLocation(city, state) {
+        this.city = city;
+        this.state = state;
+    }
+}
