18c00e9a168204f29384e56220abc85761a7c745
ritheesh4 <ritheesh.4@gmail.com>
2020-06-27 17:57:51 +0530

    try catch completed

--- 08_api_projects/81_app_weather_part4.js
+++ 08_api_projects/81_app_weather_part4.js
@@ -16,7 +16,7 @@ document.getElementById('w-change-btn').addEventListener('click',(e) => {
     const city = document.getElementById('city').value;
     const state = document.getElementById('state').value;
     // Change location    
-    weather.changeLocation(city,state);
+    weather.changeLocation(city,state); 
 
     // St location in LS
     storage.setLocationData(city,state);
