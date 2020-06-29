a47f205fe50c50fcb6b9e733d55882dad543e333
ritheesh4 <ritheesh.4@gmail.com>
2020-06-29 08:28:23 +0530

    Iterators completed

--- 10_newer_features/88_iterators.js
+++ 10_newer_features/88_iterators.js
@@ -1,17 +1,56 @@
 // Iterator exmaple
-function nameIterator(names) {
-    let nextIndex = 0;
-    return {
-        next: function () {
-            return nextIndex < names.length ?
-                { value: names[nextIndex++], done: false } :
-                { done: true }
-        }
+// function nameIterator(names) {
+//     let nextIndex = 0;
+//     return {
+//         next: function () {
+//             return nextIndex < names.length ?
+//                 { value: names[nextIndex++], done: false } :
+//                 { done: true }
+//         }
+//     }
+// }
+
+// // Create an array of naems
+// const namesArr = ['Jack', 'Jill', 'John'];
+// // Init iterator and pass in the names array
+// const names = nameIterator(namesArr);
+// console.log(names.next());
+// console.log(names.next());
+// console.log(names.next());
+// console.log(names.next());
+
+//Generator Example
+
+// function* sayNames() {
+//     yield 'Jack';
+//     yield 'Jill';
+//     yield 'John';
+// }
+
+// const name = sayNames();
+
+// console.log(name.next().value);
+// console.log(name.next().value);
+// console.log(name.next().value);
+// console.log(name.next().value);
+
+// ID Creator
+
+function* createIds() {
+    let index = 0;
+
+    while(true){
+        yield index++;
     }
 }
 
-// Create an array of naems
-const namesArr = ['Jack', 'Jill', 'John'];
-// Init iterator and pass in the names array
-const names = nameIterator(namesArr);
-console.log(names.next());
+const gen = createIds();
+
+console.log(gen.next());
+console.log(gen.next());
+console.log(gen.next());
+console.log(gen.next());
+console.log(gen.next());
+console.log(gen.next());
+console.log(gen.next());
+console.log(gen.next());
