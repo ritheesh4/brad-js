4d52e8ead172036a29bccc1119886cf7bec21949
ritheesh4 <ritheesh.4@gmail.com>
2020-07-07 17:15:02 +0530

    hangman game completed

--- 17_hangman/43_script.js
+++ 17_hangman/43_script.js
@@ -1,6 +1,6 @@
 const wordEl = document.getElementById('word');
 const wrongLettersEl = document.getElementById('wrong-letters');
-const playAgainBtn = document.getElementById('play-again');
+const playAgainBtn = document.getElementById('play-button');
 const popup = document.getElementById('popup-container');
 const notification = document.getElementById('notification-container');
 const finalMessage = document.getElementById('final-message');
@@ -39,7 +39,27 @@ function displayWord() {
 
 //Update the wrong letters
 function updateWrongLettersEl() {
-    console.log('Update worng');
+        // Display wrong letters
+    wrongLettersEl.innerHTML =`
+        ${wrongLetters.length > 0 ? '<p>Wrong</p>' : ''}
+        ${wrongLetters.map(letter => `<span>${letter}</span>`)}
+    `;
+
+    // Display parts
+    figureParts.forEach((part,index) => {
+        const errors = wrongLetters.length;
+        if(index < errors){
+            part.style.display = 'block';
+        } else {
+            part.style.display = 'none'
+        }
+    })
+
+    // check if lost
+    if(wrongLetters.length === figureParts.length) {
+        finalMessage.innerText = 'Unfortunatley you lost.😓 ';
+        popup.style.display ='flex';
+    }
 }
 
 // SHow nofification
@@ -75,6 +95,18 @@ window.addEventListener('keydown', e => {
         }
     }
 
+});
+
+// Restart game and play again
+playAgainBtn.addEventListener('click', () => {
+    // Empty arrays
+    correctedLetters.splice(0);
+    wrongLetters.splice(0);
+
+    selectedWord = words[Math.floor(Math.random() * words.length)];
+    displayWord();
+    updateWrongLettersEl();
+    popup.style.display = 'none'
 });
 
 displayWord();
