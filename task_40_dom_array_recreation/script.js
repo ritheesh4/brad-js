64a137bd5625ce9b97e909d7b5ef57bb21a2820b
ritheesh4 <ritheesh.4@gmail.com>
2020-07-10 12:50:21 +0530

    functions implemented

--- task_40_dom_array_recreation/script.js
+++ task_40_dom_array_recreation/script.js
@@ -3,9 +3,15 @@ const doubleMoney = document.getElementById('doubleMoney');
 const millionaires = document.getElementById('millionaires');
 const sortRich = document.getElementById('sortRich');
 const wealth = document.getElementById('wealth');
-const userList = document.getElementById('userData')
-const eachUser = document.getElementById('userData').querySelectorAll('li');
+const userList = document.getElementById('userData');
+let user = document.getElementsByClassName('user')
+let eachUser = document.getElementById('userData').querySelectorAll('li');
 
+let userArray = [];
+let cashArray = [];
+let doubleArray = [];
+let millionairesName = [];
+let millionairesWealth = [];
 
 // Update person and welath
 async function updateUser(e) {
@@ -13,7 +19,9 @@ async function updateUser(e) {
     const response = await fetch('https://randomuser.me/api');
     const data = await response.json();
     const userData = data.results[0];
+    userArray.push(userData);
     let cash = Math.floor(Math.random() * 1000000);
+    cashArray.push(cash);
 
     userList.innerHTML += `
     <li class = "user">
@@ -21,22 +29,56 @@ async function updateUser(e) {
     <div class="wealth">${formatMoney(cash)}</div>
     </li>
     `
+    doubleArray = [...cashArray];
 }
 
 // Double persons money
 const doublingMoney = (e) => {
+    eachUser = document.getElementById('userData').querySelectorAll('li');
+    userList.innerHTML = '';
     e.preventDefault();
-    eachUser.forEach(function(element){
-        console.log(element)
+    eachUser.forEach(function (element, index) {
+        userList.innerHTML += `
+    <li class = "user">
+    <div class="name">${userArray[index].name.first} ${userArray[index].name.last}</div>
+    <div class="wealth">${formatMoney(doubleArray[index] * 2)}</div>
+    </li>    `
+        doubleArray[index] = doubleArray[index] * 2;
     });
-    console.log("money doubled");
 }
 
 // Show the millionaires only
 const showMillionaires = (e) => {
     e.preventDefault();
     console.log("Showed millionaires");
+    indexOfMillionaires = 0;
+
+    doubleArray.forEach(function (element, index) {
+        if (element / 1000000 >= 1) {
+            console.log(element, index);
+            millionairesWealth[indexOfMillionaires] = doubleArray[index];
+            millionairesName[indexOfMillionaires] = userArray[index];
+            indexOfMillionaires++;
+        } else {
+
+        }
+    });
+
+    console.log(millionairesName, millionairesWealth)
+    eachUser = document.getElementById('userData').querySelectorAll('li');
+    userList.innerHTML = '';
+    e.preventDefault();
+    millionairesName.forEach(function (element, index) {
+        console.log(index)
+        userList.innerHTML += `
+    <li class = "user">
+    <div class="name">${millionairesName[index].name.first} ${userArray[index].name.last}</div>
+    <div class="wealth">${formatMoney(millionairesWealth[index])}</div>
+    </li>    `
+    });
 }
+
+
 
 // Sort rich first
 const sortRichFirst = (e) => {
