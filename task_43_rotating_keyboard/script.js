c7d6223b35f957ae11c51c6214c26671f3178183
ritheesh4 <ritheesh.4@gmail.com>
2020-07-14 20:35:24 +0530

    enter button next line implemented

--- task_43_rotating_keyboard/script.js
+++ task_43_rotating_keyboard/script.js
@@ -21,12 +21,10 @@ const taped = () => {
         tap++;
     } else {
         if (tap === 1) {
-            console.log(tap)
             clearAllTimeouts();
             horizontalSelection();
             tap++;
         } else {
-            console.log("key selected");
             keySelected();          
             
         }
@@ -42,7 +40,7 @@ const rotatingSection = () => {
 
     addClass(keyRow);
     let verticalSelection = setInterval(() => addClass(keyRow), 6000);
-    setTimeout(() => { clearInterval(verticalSelection); tap = 0; }, 13000);
+    // setTimeout(() => { clearInterval(verticalSelection); tap = 0; }, 13000);
 }
 
 // Add and remove the color to the selected row
@@ -55,7 +53,7 @@ const addClass = (keyRow, keyState) => {
                     element.classList.add("selecting-key");
                     setTimeout(function () {
                         element.classList.remove("selecting-key");
-                    }, 500);
+                    }, 1000);
                 }, index * 1000);
             });
         }
@@ -69,7 +67,7 @@ const addClass = (keyRow, keyState) => {
                     element.classList.add("selecting");
                     setTimeout(function () {
                         element.classList.remove("selecting");
-                    }, 500);
+                    }, 1000);
                 }, index * 1000);
             });
         } catch{
@@ -104,6 +102,7 @@ const clearAllTimeouts = () => {
 
 // Capture the selected key
 const keySelected = () => {
+    try{
     switch (selectingKey[0].getAttribute('id')) {
         case 'up':
             try {
@@ -139,15 +138,15 @@ const keySelected = () => {
             horizontalSelection();
             break;
         case 'enter':
-            textBox.querySelector('p').innerHTML += `<br>`;
+            textBox.querySelector('span').innerHTML += `<br>`;
             selectingKey[0].classList.remove("selecting-key");
             break;
         case 'space':
-            textBox.querySelector('p').innerHTML += `  `;
+            textBox.querySelector('span').innerHTML += `  `;
             selectingKey[0].classList.remove("selecting-key");
             break;
         case 'delete':
-            textBox.querySelector('p').innerHTML = textBox.querySelector('p').innerHTML.replace(/.$/, '');
+            textBox.querySelector('span').innerHTML = textBox.querySelector('span').innerHTML.replace(/.$/, '');
             selectingKey[0].classList.remove("selecting-key");
             break;
         case 'smallLetter':
@@ -155,8 +154,11 @@ const keySelected = () => {
             selectingKey[0].classList.remove("selecting-key");
             break;
         default:
-            textBox.querySelector('p').innerHTML += `${selectingKey[0].innerHTML}`;
+            textBox.querySelector('span').innerHTML += `${selectingKey[0].innerHTML}`;
             selectingKey[0].classList.remove("selecting-key");
+    }}
+    catch {
+        console.log("error to get id attribute")
     }
 }
 
