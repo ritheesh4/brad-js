e4ae36b4a4a02afc35ab74c6036ba36cd0542224
ritheesh4 <ritheesh.4@gmail.com>
2020-07-15 19:34:50 +0530

    navigation, capslock border coloring and tap button coloring changed

--- task_43_rotating_keyboard/script.js
+++ task_43_rotating_keyboard/script.js
@@ -2,6 +2,7 @@ const keyboard = document.getElementById('keyboard');
 const tapBtn = document.getElementById('tap-button');
 const selectedRw = document.getElementsByClassName('selecting');
 const selectingKey = document.getElementsByClassName('selecting-key');
+const selectedColorChange = document.getElementsByClassName('selected-color');
 const textBox = document.getElementById('text-box');
 const upBtn = document.querySelector('#up');
 const downBtn = document.querySelector('#down');
@@ -11,13 +12,11 @@ const smallLetterBtn = document.querySelector('#smallLetter');
 
 let timeouts = [];
 let tap = 0;
-let smallLetterFlag = 0;
-let verticalSelectionTime = 6000;
-let horizontalSelectionTime = 12000;
+let capsLockFlag = true;
+let selectionTimeConfig = 1; // Enter the selection time in seconds
 
-// Tap function
+// Tap function enables row selection of the keyboard
 const taped = () => {
-
     if (tap === 0) {
         rotatingSection();
         tap++;
@@ -28,12 +27,11 @@ const taped = () => {
             tap++;
         } else {
             keySelected();
-
         }
     }
-    tapBtn.style.backgroundColor = "red";
+    tapBtn.style.backgroundColor = "hsl(98, 100%, 55%)";
     tapBtn.style.color = "white";
-    setTimeout(() => { tapBtn.style.backgroundColor = "hsl(98, 100%, 55%)"; tapBtn.style.color = "black"; }, 500)
+    setTimeout(() => { tapBtn.style.border = "2px solid hsl(98, 100%, 55%)"; tapBtn.style.backgroundColor = "transparent"; tapBtn.style.color = "black"; }, 500)
 }
 
 // Rotating vertical selection;
@@ -41,36 +39,39 @@ const rotatingSection = () => {
     let keyRow = [].slice.call(keyboard.children);
 
     addClass(keyRow);
-    let verticalSelection = setInterval(() => addClass(keyRow), 6000);
+    let verticalSelection = setInterval(() => addClass(keyRow), selectionTimeConfig*6000);
     // setTimeout(() => { clearInterval(verticalSelection); tap = 0; }, 13000);
 }
 
 // Add and remove the color to the selected row
 const addClass = (keyRow, keyState) => {
 
-    if (keyState) {
+    if (keyState) { // Responsible for selection of keys
         try {
             keyRow.forEach(function (element, index) {
                 setTimeout(function () {
                     element.classList.add("selecting-key");
                     setTimeout(function () {
+                        element.classList.remove("selected-color");
                         element.classList.remove("selecting-key");
-                    }, 1000);
-                }, index * 1000);
+
+                    }, 1000*selectionTimeConfig);
+                }, index * 1000*selectionTimeConfig);
             });
         }
         catch (err) {
             console.log("Error while adding or removing class on the key")
         }
-    } else {
+    } else {  // Responsible for selection of row
         try {
             keyRow.forEach(function (element, index) {
                 setTimeout(function () {
                     element.classList.add("selecting");
                     setTimeout(function () {
+                        element.classList.remove("selected-color");
                         element.classList.remove("selecting");
-                    }, 1000);
-                }, index * 1000);
+                    }, 1000*selectionTimeConfig);
+                }, index * 1000*selectionTimeConfig);
             });
         } catch{
             console.log("Error while adding or removing class in the row")
@@ -80,12 +81,13 @@ const addClass = (keyRow, keyState) => {
 
 // Horizontal selection for keys
 const horizontalSelection = () => {
+    try { selectingKey[0].classList.remove("selected-color"); } catch{ }
     try {
         let selectedRow = [].slice.call(selectedRw[0].children);
         let keyState = true;
         addClass(selectedRow, keyState);
 
-        let horizontal = setInterval(() => addClass(selectedRow, keyState), horizontalSelectionTime);
+        let horizontal = setInterval(() => addClass(selectedRow, keyState), selectionTimeConfig*12000);
         // setTimeout(() => { clearInterval(horizontal); }, 37000);
     } catch {
         console.log("error in the horizontal selection function")
@@ -102,18 +104,27 @@ const clearAllTimeouts = () => {
     }
 }
 
-// Capture the selected key
+// Capture the selected key from the row
 const keySelected = () => {
+    selectingKey[0].classList.add("selected-color");
     try {
         switch (selectingKey[0].getAttribute('id')) {
             case 'up':
                 try {
                     let element = selectedRw[0].previousElementSibling;
                     if (element) {
+                        selectingKey[0].classList.remove("selected-color");
                         selectingKey[0].classList.remove("selecting-key");
-                        selectedRw[0].classList.remove("selecting")
+                        selectedRw[0].classList.remove("selecting");
                         element.classList.add("selecting");
-                        horizontalSelection()
+                        horizontalSelection();
+                    } else {
+                        let element = selectedRw[0].parentElement.lastElementChild;
+                        selectingKey[0].classList.remove("selected-color");
+                        selectingKey[0].classList.remove("selecting-key");
+                        selectedRw[0].classList.remove("selecting");
+                        element.classList.add("selecting");
+                        horizontalSelection();
                     }
                 }
                 catch {
@@ -126,12 +137,19 @@ const keySelected = () => {
                 try {
                     let element = selectedRw[0].nextElementSibling;
                     if (element) {
+                        selectingKey[0].classList.remove("selected-color");
                         selectingKey[0].classList.remove("selecting-key");
-                        selectedRw[0].classList.remove("selecting")
+                        selectedRw[0].classList.remove("selecting");
                         element.classList.add("selecting");
                         horizontalSelection()
+                    } else {
+                        let element = selectedRw[0].parentElement.firstElementChild;
+                        selectingKey[0].classList.remove("selected-color");
+                        selectingKey[0].classList.remove("selecting-key");
+                        selectedRw[0].classList.remove("selecting");
+                        element.classList.add("selecting");
+                        horizontalSelection();
                     }
-
                 }
                 catch {
                     console.log("end reached");
@@ -152,20 +170,20 @@ const keySelected = () => {
                 selectingKey[0].classList.remove("selecting-key");
                 break;
             case 'smallLetter':
-                smallLetterFlag = !smallLetterFlag
-                changeLetterCase(smallLetterFlag);
-                if (smallLetterFlag) {
-                    selectingKey[0].classList.add("caseChange");
-                } else {
+                capsLockFlag = !capsLockFlag
+                changeLetterCase(capsLockFlag);
+                if (capsLockFlag) {
                     selectingKey[0].classList.remove("caseChange");
+                } else {
+                    selectingKey[0].classList.add("caseChange");
                 }
-
                 selectingKey[0].classList.remove("selecting-key");
-
                 break;
             default:
                 textBox.querySelector('span').innerHTML += `${selectingKey[0].innerHTML}`;
+
                 selectingKey[0].classList.remove("selecting-key");
+
         }
     }
     catch {
@@ -174,7 +192,7 @@ const keySelected = () => {
 }
 
 // Change the case of the letter
-const changeLetterCase = (smallLetterFlag) => {
+const changeLetterCase = (capsLockFlag) => {
     let result = selectedRw[0].parentElement;
     let rowParent = [].slice.call(result.children);
 
@@ -183,7 +201,7 @@ const changeLetterCase = (smallLetterFlag) => {
         result.forEach(function (li) {
             if (li.innerHTML.substring(0, 1) !== '<') {
                 if (li.innerHTML.toUpperCase() != li.innerHTML.toLowerCase()) {
-                    if (smallLetterFlag) {
+                    if (capsLockFlag) {
                         li.innerHTML = `${li.innerHTML.toLowerCase()}`
                     } else {
                         li.innerHTML = `${li.innerHTML.toUpperCase()}`
@@ -193,11 +211,6 @@ const changeLetterCase = (smallLetterFlag) => {
         });
     });
 }
-
-
-
-// Detect when tapped on the button
-// tapBtn.addEventListener('click', taped);
 
 // Detect when tapped on the screen
 document.addEventListener("click", taped);
