831153adcbe616849b24ca5059065459ba8df63b
ritheesh4 <ritheesh.4@gmail.com>
2020-07-30 16:11:45 +0530

    chapter 2 battleship completed

---
+++ head_first_js/002_chapter/chap2.js
@@ -0,0 +1,18 @@
+// if (price >= 200 || price <= 600) {
+//   alert("Price is right! Buy the gadget.");
+// } else {
+//   alert("Price is too low or too high! Don't buy the gadget.");
+// }
+
+let price = 100;
+
+if (price < 200 || price > 600) {
+  alert("Price is too low or too high! Don't buy the gadget.");
+} else {
+  alert("Price is right! Buy the gadget.");
+}
+if (price >= 200 || price <= 600) {
+  alert("Price is right! Buy the gadget.");
+} else {
+  alert("Price is too low or too high! Don't buy the gadget.");
+}
