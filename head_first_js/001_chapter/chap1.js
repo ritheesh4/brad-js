756be137d6f2c57d7c2261dd1f227d1367200a71
ritheesh4 <ritheesh.4@gmail.com>
2020-07-30 15:42:18 +0530

    chapter 1 completed

---
+++ head_first_js/001_chapter/chap1.js
@@ -0,0 +1,16 @@
+// let total = proce - (price*(discount/100));
+
+// "Dear " + "Reader" + ","
+// "super" + "cali" + youKnowTheRest
+// phoneNumber.substring(0,3)
+
+// age < 14
+// cost >= 3.99
+// animal == "bear"
+
+// let temp = 10;
+
+// console.log((9 / 5) * temp + 32);
+
+// Doing things more than once
+
