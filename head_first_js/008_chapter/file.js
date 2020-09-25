b073a32bdf6547043e30a39816afbcd65321c119
ritheesh4 <ritheesh.4@gmail.com>
2020-09-25 10:12:54 +0530

    game and exercise completed

---
+++ head_first_js/008_chapter/file.js
@@ -0,0 +1,22 @@
+// Finishing the image game
+
+// window.onload = function () {
+//   var images = document.getElementsByTagName("img");
+//   for (var i = 0; i < images.length; i++) {
+//     images[i].onclick = showAnswer;
+//   }
+// };
+// function showAnswer(eventObj) {
+//   var image = eventObj.target;
+//   var name = image.id;
+//   name = name + ".jpg";
+//   image.src = name;
+//   setTimeout(reblur, 2000, image);
+// }
+// function reblur(image) {
+//   var name = image.id;
+//   name = name + "blur.jpg";
+//   image.src = name;
+// }
+
+
